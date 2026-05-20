68f3b066326f66f3e5fc6e4919a83367734e5dae
chetanmehra07 <chetanmehra07112003@gmail.com>
2026-05-20 23:58:06 +0530

    prepare frontend for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
 // ======================================================
 // TOKEN HANDLER
 // ======================================================
